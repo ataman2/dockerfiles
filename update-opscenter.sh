e8bb1c207b543a34d6cbb08ccc58f6ae604fe628
Piotr Bochenski <bochenski.piotr@gmail.com>
2015-11-01 18:50:25 +0100

    fix update-opscenter script to also work on osx

--- update-opscenter.sh
+++ update-opscenter.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-curl \
--X POST \
--d \
+if [ -z ${DOCKER_HOST+x} ]; then
+        export OPSIP=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' opscenter)
+else
+        export OPSIP=$(echo $DOCKER_HOST | awk -F '/' '{print $3}' | awk -F ':' '{print $1}')
+fi
+
+curl -X POST -d \
 "{
 	\"cassandra\": {
 		\"seed_hosts\": \"$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' cassandra)\"
@@ -11,4 +15,5 @@ curl \
 	\"jmx\": {
 		\"port\": \"7199\"
 	}
-}" http://$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' opscenter):8888/cluster-configs
+}" http://$OPSIP:8888/cluster-configs
+
