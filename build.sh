182c669ed59475c1d8f3df23360e4d73013bf7a8
Piotr Bochenski <bochenski.piotr@gmail.com>
2015-11-01 17:02:24 +0100

    upgrade cassandra to 2.2.3

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 docker build -t="bochen/datastax-base" datastax-base
 docker build -t="bochen/opscenter:5.2.1" opscenter
-docker build -t="bochen/cassandra:2.2.2" cassandra
+docker build -t="bochen/cassandra:2.2.3" cassandra
 docker build -t="bochen/zookeeper:3.4.6" zookeeper
 docker build -t="bochen/kafka:0.8.2.2" kafka
 docker build -t="bochen/storm-base:0.9.5" storm-base
