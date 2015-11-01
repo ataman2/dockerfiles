e361cf84323fca5a21ebd4c082ca087be742b565
Piotr Bochenski <bochenski.piotr@gmail.com>
2015-11-01 17:45:44 +0100

    better log management on services under supervisor

--- storm-base/config-supervisord.sh
+++ storm-base/config-supervisord.sh
@@ -1,6 +1,10 @@
 echo [program:storm-$1] | tee -a /etc/supervisor/conf.d/storm-$1.conf
 echo command=storm $1 | tee -a /etc/supervisor/conf.d/storm-$1.conf
 echo directory=/home/storm | tee -a /etc/supervisor/conf.d/storm-$1.conf
+echo autostart=true | tee -a /etc/supervisor/conf.d/storm-$1.conf
 echo autorestart=true | tee -a /etc/supervisor/conf.d/storm-$1.conf
 echo startsecs=15 | tee -a /etc/supervisor/conf.d/storm-$1.conf
 echo user=root | tee -a /etc/supervisor/conf.d/storm-$1.conf
+echo redirect_stderr=true | tee -a /etc/supervisor/conf.d/storm-$1.conf
+echo stdout_logfile=/dev/stdout | tee -a /etc/supervisor/conf.d/storm-$1.conf
+echo stdout_logfile_maxbytes=0 | tee -a /etc/supervisor/conf.d/storm-$1.conf
