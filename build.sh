3831b57bfdf83af2c2a719fd5146ca4d39c579be
Piotr Bochenski <bochenski.piotr@gmail.com>
2015-11-16 22:08:25 +0100

    upgrade storm to 0.9.6

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 docker build -t="bochen/datastax-base" datastax-base
-docker build -t="bochen/opscenter:5.2.1" opscenter
+docker build -t="bochen/opscenter:5.2.2" opscenter
 docker build -t="bochen/cassandra:2.2.3" cassandra
 docker build -t="bochen/zookeeper:3.4.6" zookeeper
 docker build -t="bochen/kafka:0.8.2.2" kafka
-docker build -t="bochen/storm-base:0.9.5" storm-base
-docker build -t="bochen/storm-nimbus:0.9.5" storm-nimbus
-docker build -t="bochen/storm-supervisor:0.9.5" storm-supervisor
-docker build -t="bochen/storm-ui:0.9.5" storm-ui
+docker build -t="bochen/storm-base:0.9.6" storm-base
+docker build -t="bochen/storm-nimbus:0.9.6" storm-nimbus
+docker build -t="bochen/storm-supervisor:0.9.6" storm-supervisor
+docker build -t="bochen/storm-ui:0.9.6" storm-ui
